10357685751d05adb4d3c63af14db9fb58a66630
zxywudi <39319948+zxywudi@users.noreply.github.com>
2019-08-09 18:10:01 +0800

    Add files via upload

---
+++ ActivityLostFind.java
@@ -0,0 +1,14 @@
+package com.sky.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityLostFind extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lost_find);
+    }
+}
